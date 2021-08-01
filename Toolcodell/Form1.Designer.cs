2642ce8f8dd4e28a775098903ee89fe4417e6c2e
HanaIroha <61019733+HanaIroha@users.noreply.github.com>
2021-08-01 20:06:49 +0700

    update

---
+++ Toolcodell/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace Toolcodell
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(22, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 27);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Bắt đầu";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(175, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 27);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Dừng";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tổng mã: 0/0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 82);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Tool Sự Kiện Sinh Nhật LOL - Iroha";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Toolcodell/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Toolcodell
+{
+    public partial class Form1 : Form
+    {
+        static List<string> logList;
+        static int index = 0;
+        static int allz;
+        Thread th;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+        [DllImport("user32.dll")]
+        internal static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        [DllImport("user32.dll")]
+        static extern bool ClientToScreen(IntPtr hWnd, ref Point lpPoint);
+
+        [DllImport("user32.dll")]
+        internal static extern uint SendInput(uint nInputs, [MarshalAs(UnmanagedType.LPArray), In] INPUT[] pInputs, int cbSize);
+
+#pragma warning disable 649
+        internal struct INPUT
+        {
+            public UInt32 Type;
+            public MOUSEKEYBDHARDWAREINPUT Data;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        internal struct MOUSEKEYBDHARDWAREINPUT
+        {
+            [FieldOffset(0)]
+            public MOUSEINPUT Mouse;
+        }
+
+        internal struct MOUSEINPUT
+        {
+            public Int32 X;
+            public Int32 Y;
+            public UInt32 MouseData;
+            public UInt32 Flags;
+            public UInt32 Time;
+            public IntPtr ExtraInfo;
+        }
+
+#pragma warning restore 649
+
+
+        public static void ClickOnPoint(IntPtr wndHandle, Point clientPoint)
+        {
+            var oldPos = Cursor.Position;
+
+            /// get screen coordinates
+            ClientToScreen(wndHandle, ref clientPoint);
+
+            /// set cursor on coords, and press mouse
+            Cursor.Position = new Point(clientPoint.X, clientPoint.Y);
+
+            var inputMouseDown = new INPUT();
+            inputMouseDown.Type = 0; /// input type mouse
+            inputMouseDown.Data.Mouse.Flags = 0x0002; /// left button down
+
+            var inputMouseUp = new INPUT();
+            inputMouseUp.Type = 0; /// input type mouse
+            inputMouseUp.Data.Mouse.Flags = 0x0004; /// left button up
+
+            var inputs = new INPUT[] { inputMouseDown, inputMouseUp };
+            SendInput((uint)inputs.Length, inputs, Marshal.SizeOf(typeof(INPUT)));
+
+            /// return mouse 
+            Cursor.Position = oldPos;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Action DoWorkTool = () =>
+            {
+                var all = Process.GetProcessesByName("LeagueClientUx");
+                IntPtr MainWindowz = all[0].MainWindowHandle;
+                foreach (var z in all)
+                {
+                    IntPtr h = z.MainWindowHandle;
+                    ShowWindow(h, 10);
+                    SetForegroundWindow(h);
+                    MainWindowz = h;
+                }
+                for (; ; )
+                {
+                    label1.Text = "Tổng mã: " + index + "/" + allz;
+                    Thread.Sleep(500);
+                    if (index < allz)
+                    {
+                        ClickOnPoint(MainWindowz, new Point(617, 531));
+                        Thread.Sleep(500);
+                        SendKeys.SendWait("^{a}");
+                        Clipboard.Clear();
+                        Clipboard.SetText(logList[index]);
+                        index++;
+                        Thread.Sleep(250);
+                        SendKeys.SendWait("^{v}");
+                        Thread.Sleep(500);
+                        ClickOnPoint(MainWindowz, new Point(773, 526));
+                        Thread.Sleep(500);
+                        ClickOnPoint(MainWindowz, new Point(485, 430));
+                        Thread.Sleep(500);
+                        ClickOnPoint(MainWindowz, new Point(485, 430));
+                    }
+                    else
+                    {
+                        MessageBox.Show("Đã hết mã!");
+                        break;
+                    }
+                }
+            };
+            th = new Thread(new ThreadStart(DoWorkTool));
+            th.SetApartmentState(ApartmentState.STA);
+            th.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            th.Abort();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var logFile = File.ReadAllLines("code.txt");
+            logList = new List<string>(logFile);
+            allz = logList.Count();
+            label1.Text = "Tổng mã: 0/"+ allz;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                th.Abort();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
